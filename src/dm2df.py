df8536e81e898a01606e736628bdb52531afa495
andrea.grisafi <andrea.grisafi91@gmail.com>
2020-06-24 19:37:53 +0200

    Hartree and external energy calculation included

--- src/dm2df.py
+++ src/dm2df.py
@@ -126,163 +126,18 @@ np.save("overlaps/overlap_conf"+str(iconf)+".npy",Over)
 
 # --------------------------------------------------
 
-print "Computing ab-initio electrostatic energy..."
+print "Computing ab-initio energies.."
 
 # Hartree energy
 J = np.einsum('Q,mnQ->mn', rho, eri3c)
 e_h = np.einsum('ij,ji', J, dm) * 0.5
-
-# Nuclear energy
-E_NN=0
-for I in range(auxmol.natm):   
-    for J in range(I):
-        if I != J:
-            DIJ=np.linalg.norm(np.array(auxmol._atom[I][1])-np.array(auxmol._atom[J][1]))
-            E_NN+=auxmol._atm[I][0]*auxmol._atm[J][0]/DIJ
+f = open("qm-runs/conf_"+str(iconf+1)+"/hartree_energy.dat", 'w') 
+print >> f, e_h
+f.close()
 
 # Nuclear-electron energy
 h = mol.intor_symmetric('int1e_nuc')
 e_Ne = np.einsum('ij,ji', h, dm) 
-
-with open("qm-runs/conf_"+str(iconf+1)+"/QM-electro.dat", 'w') as file:
-    file.write("E_NN: "+str(E_NN)+"\n")
-    file.write("E_Ne: "+str(e_Ne)+"\n")
-    file.write("E_ee: "+str(e_h)+"\n")
-    file.write("Electrostatic energy:"+str(E_NN+e_Ne+e_h)+"\n")
-
-# Density-fitted electrostatic energy
-print "Computing density-fitted electrostatic energy..."
-
-matrix_of_coefficients=np.outer(rho,rho)
-e_h1=np.einsum('ij,ji',eri2c,matrix_of_coefficients)*0.5
-
-def int_D0(symb,l,i):
-    alpha=auxmol._basis[symb][i][1][0]
-    sigma = np.sqrt(1.0/(2*alpha))
-    inner = 0.5*special.gamma(l+1.5)*(sigma**2)**(l+1.5)
-    return (0.5*(alpha**(-1-l/2))*special.gamma(1+l/2))/ np.sqrt(inner)
-
-def int_D1(r,symb,l,i):
-    alpha=auxmol._basis[symb][i][1][0]
-    sigma = np.sqrt(1.0/(2*alpha))
-    inner = 0.5*special.gamma(l+1.5)*(sigma**2)**(l+1.5)
-    return ((r**(3 + 2*l)*(alpha*r**2)**(-1.5 - l)*(special.gamma(1.5 + l) - special.gamma(1.5 + l)*special.gammaincc(1.5 + l,alpha*r**2)))/2.)/np.sqrt(inner)
-
-def int_D2(r,symb,l,i):
-    alpha=auxmol._basis[symb][i][1][0]
-    sigma = np.sqrt(1.0/(2*alpha))
-    inner = 0.5*special.gamma(l+1.5)*(sigma**2)**(l+1.5)
-    return (np.exp(-alpha*r**2)/(2*alpha))/np.sqrt(inner)
-
-def complex_to_real_transformation(sizes):
-
-    matrices = []
-    for i in range(len(sizes)):
-        lval = (sizes[i]-1)/2
-        st = (-1.0)**(lval+1)
-        transformation_matrix = np.zeros((sizes[i],sizes[i]),dtype=complex)
-        for j in range( int((sizes[i]-1)/2) ):
-            transformation_matrix[j][j] = 1.0j
-            transformation_matrix[j][sizes[i]-j-1] = st*1.0j
-            transformation_matrix[sizes[i]-j-1][j] = 1.0
-            transformation_matrix[sizes[i]-j-1][sizes[i]-j-1] = st*-1.0
-            st = st * -1.0
-        transformation_matrix[int((sizes[i]-1)/2)][int((sizes[i]-1)/2)] = np.sqrt(2.0)
-        transformation_matrix /= np.sqrt(2.0)
-        matrices.append(transformation_matrix)
-
-    return matrices
-
-def real_wigner(dij,lam):
-    theta = np.arccos(dij[2]/np.linalg.norm(dij))
-    phi = np.arctan2(dij[1],dij[0])
-    wigner = np.zeros(2*lam+1,complex)
-    for mu in range(2*lam+1):
-        mu1=mu-lam
-        wigner[mu] = np.sqrt(4*np.pi/(2*lam+1))*np.conj(special.sph_harm(mu1,lam,phi,theta))
-    CC = np.conj(complex_to_real_transformation([2*lam+1])[0])
-    return np.dot(CC,wigner)
-
-
-nat=len(mol._atm)
-i=0
-j=0
-coeffs=np.zeros(len(rho),dtype=float)
-for iat in range(nat):
-    atom_symb=auxmol._atom[iat][0]
-    max_l=auxmol._basis[atom_symb][-1][0]
-    numbs = [x[0] for x in auxmol._basis[auxmol._atom[iat][0]]]
-    for l in range(max_l+1):
-        n_l = numbs.count(l)
-        for n in range(n_l):
-            for m in range(2*l+1):
-                if l ==1 :
-                    if m != 0:
-                        coeffs[i-1]=rho[i]
-                        i+=1
-                    else:
-                        coeffs[i+2]=rho[i]
-                        i+=1
-                else:
-                    coeffs[i]=rho[i]
-                    i+=1
-                
-
-e_el=0
-jdx=0
-for I in range(auxmol.natm):   
-    kdx=0
-    contr3=0
-    for J in range(auxmol.natm):
-        if I==J:
-            atm_symbol=auxmol._atom[J][0]
-            lmax_J=auxmol._basis[atm_symbol][-1][0]
-            numbs = [x[0] for x in auxmol._basis[atm_symbol]]
-            idx=0
-            for l in range(lmax_J+1):
-                n_l = numbs.count(l)
-                for n in range(n_l):
-                    if l==0:
-                        integral=int_D0(atm_symbol,l,idx)
-                        idx+=1
-                    else:
-                        integral=0
-                        idx+=1
-                    for m in range(2*l+1):
-                        contr3 += coeffs[jdx]*integral
-                        jdx+=1
-                        kdx+=1
-        else:
-            d_vec_IJ=np.array(auxmol._atom[I][1])-np.array(auxmol._atom[J][1])
-            DIJ=np.linalg.norm(np.array(auxmol._atom[I][1])-np.array(auxmol._atom[J][1]))
-            atm_symbol=auxmol._atom[J][0]
-            lmax_J=auxmol._basis[atm_symbol][-1][0]
-            numbs = [x[0] for x in auxmol._basis[atm_symbol]]
-            idx=0
-            for l in range(lmax_J+1):
-                n_l = numbs.count(l)
-                contr2=0
-                for n in range(n_l):
-                    integral1=int_D1(DIJ,atm_symbol,l,idx)
-                    integral2=int_D2(DIJ,atm_symbol,l,idx)
-    
-                    integral1=integral1*1/(DIJ**(l+1))
-                    integral2=integral2*(DIJ**(l))
-            
-                    idx+=1
-                    W=np.real(real_wigner(d_vec_IJ,l))
-                    contr = 0.0
-                    for m in range(2*l+1):
-                        contr+= coeffs[kdx]*W[m]
-                        kdx+=1
-                    contr2 += contr*(integral1+integral2)
-                contr3 += contr2 * np.sqrt(1/(2*l+1))    
-    e_el += contr3* auxmol._atm[I][0]
-
-e_el=-e_el*np.sqrt(4*np.pi)
-
-with open("qm-runs/conf_"+str(iconf+1)+"/RI-electro.dat", 'w') as file:
-    file.write("E_NN: "+str(E_NN)+"\n")
-    file.write("E_Ne: "+str(e_el)+"\n")
-    file.write("E_ee: "+str(e_h1)+"\n")
-    file.write("Electrostatic energy:"+str(E_NN+e_el+e_h1)+"\n")
+f = open("qm-runs/conf_"+str(iconf+1)+"/external_energy.dat", 'w') 
+print >> f, e_Ne
+f.close()
---
+++ src/energy.py
@@ -0,0 +1,193 @@
+import sys
+import time
+import numpy as np
+from pyscf import gto
+from ase.io import read
+from scipy import special
+
+import basis
+
+sys.path.insert(0, './')
+import inp
+
+# read species
+spelist = inp.species
+spe_dict = {}
+for i in xrange(len(spelist)):
+    spe_dict[i] = spelist[i]
+
+# read basis
+[lmax,nmax] = basis.basiset(inp.basis)
+
+# read system
+xyzfile = read(inp.filename,":")
+ndata = len(xyzfile)
+
+bohr2ang = 0.52917721067121
+#======================= system parameters
+atomic_symbols = []
+atomic_valence = []
+natoms = np.zeros(ndata,int) 
+for i in xrange(len(xyzfile)):
+    atomic_symbols.append(xyzfile[i].get_chemical_symbols())
+    atomic_valence.append(xyzfile[i].get_atomic_numbers())
+    natoms[i] = int(len(atomic_symbols[i]))
+natmax = max(natoms)
+
+# load predicted coefficients for test structures
+trainrangetot = np.loadtxt("training_set.txt",int)
+testrange = np.setdiff1d(range(ndata),trainrangetot)
+ntest = len(testrange)
+natoms_test = natoms[testrange]
+
+def complex_to_real_transformation(sizes):
+    """Transformation matrix from complex to real spherical harmonics"""
+    matrices = []
+    for i in xrange(len(sizes)):
+        lval = (sizes[i]-1)/2
+        st = (-1.0)**(lval+1)
+        transformation_matrix = np.zeros((sizes[i],sizes[i]),dtype=complex)
+        for j in xrange( (sizes[i]-1)/2 ):
+            transformation_matrix[j][j] = 1.0j
+            transformation_matrix[j][sizes[i]-j-1] = st*1.0j
+            transformation_matrix[sizes[i]-j-1][j] = 1.0
+            transformation_matrix[sizes[i]-j-1][sizes[i]-j-1] = st*-1.0
+            st = st * -1.0
+        transformation_matrix[(sizes[i]-1)/2][(sizes[i]-1)/2] = np.sqrt(2.0)
+        transformation_matrix /= np.sqrt(2.0)
+        matrices.append(transformation_matrix)
+    return matrices
+
+def sph_zproj(theta,phi,lam):
+    """Project on the spherical harmonics evaluated at d_ij direction"""
+    sph_dij = np.zeros(2*lam+1,complex)
+    for mu in xrange(2*lam+1):
+        sph_dij[mu] = special.sph_harm(mu-lam,lam,phi,theta)
+    CR = complex_to_real_transformation([2*lam+1])[0]
+    return np.real(np.dot(CR,sph_dij))
+
+def radint0(alp):
+    """Centered Coulomb integral from 0 to infinity"""
+    sigma = np.sqrt(1.0/(2*alp))
+    inner = 0.5*special.gamma(1.5)*(sigma**2)**(1.5)
+    rint0 = 0.5/alp
+    return rint0/np.sqrt(inner)
+
+def radint1(lval,alp,d):
+    """Coulomb-Legendre integral from 0 to dij"""
+    sigma = np.sqrt(1.0/(2*alp))
+    inner = 0.5*special.gamma(lval+1.5)*(sigma**2)**(lval+1.5)
+    rint1 = 0.5*alp**(-1.5-lval)*(special.gamma(1.5+lval)
+            -special.gamma(1.5 + lval)*special.gammaincc(lval+1.5,alp*d**2))
+    return rint1/np.sqrt(inner)
+
+def radint2(lval,alp,d):
+    """Coulomb-Legendre integral from dij to infinity"""
+    sigma = np.sqrt(1.0/(2*alp))
+    inner = 0.5*special.gamma(lval+1.5)*(sigma**2)**(lval+1.5)
+    rint2 = 0.5*np.exp(-alp*d**2)/alp
+    return rint2/np.sqrt(inner)
+
+coeffs = np.load("predictions.npy")
+
+av_coefs = {}
+for spe in spelist:
+    av_coefs[spe] = np.load("averages_"+str(spe)+".npy")
+
+f = open("hartree_energy.dat","w")
+g = open("external_energy.dat","w")
+print "Computing Hartree and external energy..."
+itest=0
+for iconf in testrange:
+    atoms = atomic_symbols[iconf]
+    valences = atomic_valence[iconf]
+    nele = np.sum(valences)
+    natoms = len(atoms)
+    # Load projections and overlaps
+    for jat in xrange(natoms):
+       spe = atoms[jat]
+       for n in xrange(nmax[(spe,0)]):
+           coeffs[itest,jat,0,n,0] += av_coefs[atoms[jat]][n]
+    # Compute Hartree energy
+    projs = np.load("projections/projections_conf"+str(iconf)+".npy")
+    overl = np.load("overlaps/overlap_conf"+str(iconf)+".npy")
+    rcoeffs = np.linalg.solve(overl,projs)
+    ref_coeffs = np.zeros_like(coeffs)
+    rho = np.zeros(projs.shape,float)
+    ref_rho = np.zeros(projs.shape,float)
+    icoeff = 0
+    for iat in xrange(natoms):
+        for l in xrange(lmax[atoms[iat]]+1):
+            for n in xrange(nmax[(atoms[iat],l)]):
+                for im in xrange(2*l+1):
+                    ref_coeffs[itest,iat,l,n,im] = rcoeffs[icoeff]
+                    if l==1:
+                        if im != 2:
+                           ref_rho[icoeff+1] = ref_coeffs[itest,iat,l,n,im]
+                           rho[icoeff+1] = coeffs[itest,iat,l,n,im]
+                        elif im == 2:
+                           ref_rho[icoeff-2] = ref_coeffs[itest,iat,l,n,im]
+                           rho[icoeff-2] = coeffs[itest,iat,l,n,im]
+
+                    else:
+                        ref_rho[icoeff] = ref_coeffs[itest,iat,l,n,im]
+                        rho[icoeff] = coeffs[itest,iat,l,n,im]
+                    icoeff +=1
+    geom = read("qm-runs/conf_"+str(iconf+1)+"/coords.xyz")
+    coords = geom.get_positions()
+    catoms = []
+    for i in xrange(natoms):
+        coord = coords[i]
+        catoms.append([atoms[i],(coord[0],coord[1],coord[2])])
+    coords /= bohr2ang
+    auxmol = gto.M(atom=catoms,basis="cc-pvqz jkfit")
+    eri2c = auxmol.intor('int2c2e_sph')
+    matrix_of_coefficients = np.outer(rho,rho)
+    E_H = np.einsum('ij,ji',eri2c,matrix_of_coefficients)*0.5
+    matrix_of_coefficients = np.outer(ref_rho,ref_rho)
+    E_H_ref = np.einsum('ij,ji',eri2c,matrix_of_coefficients)*0.5
+    E_H_ab = np.loadtxt("qm-runs/conf_"+str(iconf+1)+"/hartree_energy.dat")
+    # Compute electron-nuclear energy
+    E_eN = 0.0
+    E_eN_ref = 0.0
+    for iat in xrange(natoms):
+        contr = 0.0
+        ref_contr = 0.0
+        for jat in xrange(natoms):
+            spe = atoms[jat]
+            if iat==jat:
+                idx=0
+                for n in xrange(nmax[(spe,0)]):
+                    alpha=auxmol._basis[spe][idx][1][0]
+                    r0 = radint0(alpha)
+                    contr += coeffs[itest,jat,0,n,0]*r0
+                    ref_contr += ref_coeffs[itest,jat,0,n,0]*r0
+                    idx+=1
+            else:
+                dist = coords[iat]-coords[jat]
+                dij  = np.linalg.norm(dist)
+                theta = np.arccos(dist[2]/dij)
+                phi = np.arctan2(dist[1],dist[0])
+                idx=0
+                for l in xrange(lmax[spe]+1):
+                    wigner_real = sph_zproj(theta,phi,l)*np.sqrt(4.0*np.pi)/float(2*l+1)
+                    for n in xrange(nmax[(spe,l)]):
+                        alpha=auxmol._basis[spe][idx][1][0]
+                        r1 = radint1(l,alpha,dij)/(dij**(l+1))
+                        r2 = radint2(l,alpha,dij)*(dij**l)
+                        rotated_coeffs = np.dot(wigner_real,coeffs[itest,jat,l,n,:2*l+1])
+                        contr += (r1 + r2)*rotated_coeffs
+                        rotated_coeffs = np.dot(wigner_real,ref_coeffs[itest,jat,l,n,:2*l+1])
+                        ref_contr += (r1 + r2)*rotated_coeffs
+                        idx+=1
+        E_eN += contr*valences[iat]
+        E_eN_ref += ref_contr*valences[iat]
+    E_eN *= -np.sqrt(4.0*np.pi)
+    E_eN_ref *= -np.sqrt(4.0*np.pi)
+    E_eN_ab = np.loadtxt("qm-runs/conf_"+str(iconf+1)+"/external_energy.dat")
+    print >> f, iconf+1, E_H_ab, E_H_ref, E_H
+    print >> g, iconf+1, E_eN_ab, E_eN_ref, E_eN
+    itest+=1
+
+f.close()
+g.close()
