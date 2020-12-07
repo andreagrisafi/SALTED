1ef27a758eb79bfd6edb2421c7417479cfdaed3c
andrea.grisafi <andrea.grisafi91@gmail.com>
2020-12-07 16:54:31 +0100

    update

--- examples/water_predictions/runall.sh
+++ examples/water_predictions/runall.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash
 
-#python $RHOMLPATH/sparse_set.py 
-#python $RHOMLPATH/kernel_mm.py 
-#python $RHOMLPATH/kernel_nm.py 
-#python $RHOMLPATH/initialize.py
-#python $RHOMLPATH/matrices.py
-#python $RHOMLPATH/learn.py 
-#python $RHOMLPATH/kernel_tm.py 
-#python $RHOMLPATH/predict.py 
-#python $RHOMLPATH/error_prediction.py
-
+python $RHOMLPATH/sparse_set.py 
+python $RHOMLPATH/kernel_mm.py 
+python $RHOMLPATH/kernel_nm.py 
+python $RHOMLPATH/initialize.py
 python $RHOMLPATH/matrices.py
 python $RHOMLPATH/learn.py 
-python $RHOMLPATH/validate.py 
-python $RHOMLPATH/error_validation.py
+
+# OUT-OF-SAMPLE PREDICTION
+python $RHOMLPATH/kernel_tm.py 
+python $RHOMLPATH/predict.py 
+python $RHOMLPATH/error_prediction.py
+
+# IN-SAMPLE VALIDATION
+#python $RHOMLPATH/validate.py 
+#python $RHOMLPATH/error_validation.py
