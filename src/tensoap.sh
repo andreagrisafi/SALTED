f394a4d6149884d967b50f21a40fdb6c0ce07f54
Alan Lewis <lewisala@mpsd-hpc-login1.desy.de>
2022-07-21 10:44:53 +0200

    Add MAE script, allow dense SOAP calculation

--- src/tensoap.sh
+++ src/tensoap.sh
@@ -1,9 +1,16 @@
 for (( lam=0; lam<=$lmax; lam++ )); do
 
+if [ $TENSOAP_NC -ne 0 ]; then
 #RUN TO SELECT nc SPARSE FEATURES FROM ns RANDOM STRUCTURES
 get_power_spectrum.py -f $TENSOAP_FILE_IN -lm ${lam} $TENSOAP_P -s $TENSOAP_SPECIES -c $TENSOAP_SPECIES -nc $TENSOAP_NC -ns $TENSOAP_NS -sm 'random' -o $TENSOAP_OUTDIR/FEAT-${lam}
 
 #RUN TO COMPUTE FEATURES WITH SPARSE FEATURES PRESELECTED
 get_power_spectrum.py -f $TENSOAP_FILE_IN -lm ${lam} $TENSOAP_P -s $TENSOAP_SPECIES -c $TENSOAP_SPECIES -sf $TENSOAP_OUTDIR/FEAT-${lam} -o $TENSOAP_OUTDIR/FEAT-${lam}
+
+else
+
+get_power_spectrum.py -f $TENSOAP_FILE_IN -lm ${lam} $TENSOAP_P -s $TENSOAP_SPECIES -c $TENSOAP_SPECIES -o $TENSOAP_OUTDIR/FEAT-${lam}
+
+fi
 
 done
