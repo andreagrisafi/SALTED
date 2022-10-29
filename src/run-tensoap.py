ca7fe63a2298cd1bb8d75dd79f3e8fe555bbd583
Alan Lewis <lewisala@mpsd-hpc-login1.desy.de>
2022-10-29 12:48:16 +0200

    Calculate descriptors in parallel

--- src/run-tensoap.py
+++ src/run-tensoap.py
@@ -1,5 +1,6 @@
 import os
 from sys_utils import read_system
+from math import ceil
 import inp
 import subprocess
 import argparse
@@ -14,6 +15,7 @@ def add_command_line_arguments_contraction(parsetext):
     parser.add_argument("-rc", "--rcut",       type=float, default=4.0,                              help="Specify the SOAP cutoff radius")
     parser.add_argument("-sg", "--sigma",         type=float, default=0.3,                              help="Gaussian width")
     parser.add_argument("-d", "--dummy", type=int,default=0, help="Include a dummy atom in the SOAP descriptor, at -x (1), -y (2), or -z (3)")
+    parser.add_argument("--parallel", type=int,default=0, help="Trivially parallelise the calculation of TENSOAP descriptors")
     args = parser.parse_args()
     return args
 
@@ -26,16 +28,19 @@ vf = args.vol_frac
 rc = args.rcut
 sg = args.sigma
 dummy = args.dummy
+parallel = args.parallel
 
 spelist, lmax, nmax, llmax, nnmax, ndata, atomic_symbols, natoms, natmax = read_system()
 if predict:
     dirpath = os.path.join(inp.path2ml, inp.predict_soapdir)
-    os.environ['TENSOAP_FILE_IN'] = inp.predict_filename
+    fname = inp.predict_filename
 else:
     dirpath = os.path.join(inp.path2ml, inp.soapdir)
-    os.environ['TENSOAP_FILE_IN'] = inp.filename
+    fname = inp.filename
 
+#os.environ['TENSOAP_FILE_IN'] = fname
 os.environ['lmax'] = str(llmax)
+#os.environ['lmax'] = '1' #TEST
 os.environ['TENSOAP_OUTDIR'] = dirpath
 os.environ['TENSOAP_SPECIES'] = ' '.join(inp.species)
 os.environ['TENSOAP_NC'] = str(nc)
@@ -60,4 +65,52 @@ if not os.path.exists(inp.path2ml):
 if not os.path.exists(dirpath):
     os.mkdir(dirpath)
 
-subprocess.call(['bash',spath+'/tensoap.sh'])
+if parallel > 0:
+    import ase.io
+    import numpy as np
+    coords = ase.io.read(fname,":")
+    npoints = len(coords)
+    block = ceil(npoints/parallel)
+    output = [None]*parallel
+    if nc > 0:
+        for l in range(llmax+1):
+            output[l] = subprocess.call(['bash',spath+'/tensoap_sparsify.sh',fname,str(l)])
+#       for l in range(llmax+1):
+#           output[l].communicate()
+
+
+    for i in range(parallel):
+        fname1 = str(i)+'_'+fname
+        if i < parallel-1:
+           ase.io.write(fname1,coords[i*block:(i+1)*block])
+        else:
+           ase.io.write(fname1,coords[i*block:])
+#        os.environ['TENSOAP_FILE_IN'] = fname1
+        output[i] = subprocess.Popen(['bash',spath+'/tensoap.sh',fname1,str(i)])
+
+    # Wait for all blocks to finish
+    for i in range(parallel):
+        output[i].wait()
+
+    # Clean Up
+    for i in range(parallel):
+        fname1 = str(i)+'_'+fname
+        os.remove(fname1)
+    for l in range(llmax+1):
+        for i in range(parallel):
+            block = np.load(dirpath+str(i)+'FEAT-'+str(l)+'.npy')
+            natoms = np.load(dirpath+str(i)+'FEAT-'+str(l)+'_natoms.npy')
+            if i == 0:
+                full = block
+                full_natoms = natoms
+            else:
+                full = np.concatenate([full,block])
+                full_natoms = np.concatenate([full_natoms,natoms])
+            os.remove(dirpath+str(i)+'FEAT-'+str(l)+'.npy')
+            os.remove(dirpath+str(i)+'FEAT-'+str(l)+'_natoms.npy')
+        np.save(dirpath+'FEAT-'+str(l)+'.npy',full)
+        np.save(dirpath+'FEAT-'+str(l)+'_natoms.npy',full_natoms)
+
+
+else:
+    subprocess.call(['bash',spath+'/tensoap.sh',fname])
--- src/tensoap.sh
+++ src/tensoap.sh
@@ -1,15 +1,18 @@
+# $1 == $TENSOAP_FILE_IN 
+# $2 == parallel task index
+
 for (( lam=0; lam<=$lmax; lam++ )); do
 
 if [ $TENSOAP_NC -ne 0 ]; then
 #RUN TO SELECT nc SPARSE FEATURES FROM ns RANDOM STRUCTURES
-get_power_spectrum.py -f $TENSOAP_FILE_IN -lm ${lam} $TENSOAP_P $TENSOAP_VF -s $TENSOAP_SPECIES -c $TENSOAP_SPECIES -nc $TENSOAP_NC -ns $TENSOAP_NS -sm 'random' -o $TENSOAP_OUTDIR/FEAT-${lam} -rc $TENSOAP_RC -sg $TENSOAP_SG $TENSOAP_D
+#get_power_spectrum.py -f $1 -lm ${lam} $TENSOAP_P $TENSOAP_VF -s $TENSOAP_SPECIES -c $TENSOAP_SPECIES -nc $TENSOAP_NC -ns $TENSOAP_NS -sm 'random' -o $TENSOAP_OUTDIR/$2FEAT-${lam} -rc $TENSOAP_RC -sg $TENSOAP_SG $TENSOAP_D
 
 #RUN TO COMPUTE FEATURES WITH SPARSE FEATURES PRESELECTED
-get_power_spectrum.py -f $TENSOAP_FILE_IN -lm ${lam} $TENSOAP_P $TENSOAP_VF -s $TENSOAP_SPECIES -c $TENSOAP_SPECIES -sf $TENSOAP_OUTDIR/FEAT-${lam} -o $TENSOAP_OUTDIR/FEAT-${lam} -rc $TENSOAP_RC -sg $TENSOAP_SG $TENSOAP_D
+get_power_spectrum.py -f $1 -lm ${lam} $TENSOAP_P $TENSOAP_VF -s $TENSOAP_SPECIES -c $TENSOAP_SPECIES -sf $TENSOAP_OUTDIR/FEAT-${lam} -o $TENSOAP_OUTDIR/$2FEAT-${lam} -rc $TENSOAP_RC -sg $TENSOAP_SG $TENSOAP_D
 
 else
 
-get_power_spectrum.py -f $TENSOAP_FILE_IN -lm ${lam} $TENSOAP_P $TENSOAP_VF -s $TENSOAP_SPECIES -c $TENSOAP_SPECIES -o $TENSOAP_OUTDIR/FEAT-${lam} -rc $TENSOAP_RC -sg $TENSOAP_SG $TENSOAP_D
+get_power_spectrum.py -f $1 -lm ${lam} $TENSOAP_P $TENSOAP_VF -s $TENSOAP_SPECIES -c $TENSOAP_SPECIES -o $TENSOAP_OUTDIR/$2FEAT-${lam} -rc $TENSOAP_RC -sg $TENSOAP_SG $TENSOAP_D
 
 fi
 
