ec3d791539a2bfa414761c7303f3acf87afb9018
Alan Lewis <alewis@raven.mpcdf.mpg.de>
2023-09-02 20:09:17 +0200

    Add aims helper functions

---
+++ salted/aims/collect_errors.py
@@ -0,0 +1,106 @@
+import numpy as np
+import sys
+import inp
+import argparse
+from os import listdir
+
+def add_command_line_arguments_contraction(parsetext):
+    parser = argparse.ArgumentParser(description=parsetext)
+    parser.add_argument("-pr", "--predict", action='store_true', help="Prepare geometries for a true prediction")
+    args = parser.parse_args()
+    return args
+
+args = add_command_line_arguments_contraction("")
+predict = args.predict
+
+if predict:
+    dn = inp.path2qm+inp.predict_data
+    l = listdir(dn+'geoms/')
+    nfiles = len(l)
+    testset = list(range(nfiles))
+    testset = [x+1 for x in testset]
+else:
+    dn = inp.path2qm+'data/'
+    trainset = np.loadtxt('training_set.txt')
+    l = listdir(dn+'geoms/')
+    nfiles = len(l)
+    testset = np.setdiff1d(range(nfiles),trainset)+1
+
+es = []
+xcs = []
+eles = []
+n_atoms = []
+
+
+for k,i in enumerate(testset):
+    e = []
+    xc = []
+    har = []
+    ele = []
+    dirn = dn+str(i)+'/'
+    
+    f1 = open(dirn+'aims.out')
+    for line in f1:
+        if line.find('| Number of atoms') != -1:
+           n_atoms.append(line.split()[5])
+        elif line.find('| Electrostatic energy') != -1:
+            ele.append(line.split()[6])
+        elif line.find('XC energy correction') != -1:
+            xc.append(line.split()[7])
+        elif line.find('| Electronic free energy per atom') != -1:
+            e.append(line.split()[7])
+        else:
+            continue
+
+    if predict:
+        f1 = open(dirn+'aims_predict.out')
+    else:
+        f1 = open(dirn+'aims_validate.out')
+    for line in f1:
+        if line.find('| Electrostatic energy') != -1:
+            ele.append(line.split()[6])
+        elif line.find('XC energy correction') != -1:
+            xc.append(line.split()[7])
+        elif line.find('| Electronic free energy per atom') != -1:
+            e.append(line.split()[7])
+        else:
+            continue
+
+    es.append([])
+    xcs.append([])
+    eles.append([])
+    es[k].append(e[-2])
+    xcs[k].append(xc[-2])
+    eles[k].append(ele[-2])
+    es[k].append(e[-1])
+    xcs[k].append(xc[-1])
+    eles[k].append(ele[-1])
+    
+es = np.array(es,dtype = float)
+xcs = np.array(xcs,dtype = float)
+eles = np.array(eles,dtype = float)
+n_atoms = np.array(n_atoms,dtype = float)
+
+for i in range(2):
+    xcs[:,i] /= n_atoms
+    eles[:,i] /= n_atoms
+
+if predict:
+    np.savetxt('predict_reference_electrostatic_energy.dat',eles[:,0])
+    np.savetxt('predict_reference_xc_energy.dat',xcs[:,0])
+    np.savetxt('predict_reference_total_energy.dat',es[:,0])
+    np.savetxt('prediction_electrostatic_energy.dat',eles[:,1])
+    np.savetxt('prediction_xc_energy.dat',xcs[:,1])
+    np.savetxt('prediction_total_energy.dat',es[:,1])
+else:
+    np.savetxt('val_reference_electrostatic_energy.dat',eles[:,0])
+    np.savetxt('val_reference_xc_energy.dat',xcs[:,0])
+    np.savetxt('val_reference_total_energy.dat',es[:,0])
+    np.savetxt('validation_electrostatic_energy.dat',eles[:,1])
+    np.savetxt('validation_xc_energy.dat',xcs[:,1])
+    np.savetxt('validation_total_energy.dat',es[:,1])
+
+print('Mean absolute errors (eV/atom):')
+print('Electrostatic energy:',np.average(np.abs(eles[:,1]-eles[:,0])))
+print('XC energy:',np.average(np.abs(xcs[:,1]-xcs[:,0])))
+print('Total energy:',np.average(np.abs(es[:,1]-es[:,0])))
---
+++ salted/aims/move_data.py
@@ -0,0 +1,72 @@
+import numpy as np
+import os
+import inp
+from ase.io import read
+
+if inp.parallel:
+    from mpi4py import MPI
+    # MPI information
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+    print('This is task',rank+1,'of',size,flush=True)
+else:
+    rank = 0
+
+if (rank == 0):
+    if not os.path.exists(inp.path2qm+inp.ovlpdir):
+        os.mkdir(inp.path2qm+inp.ovlpdir)
+    if not os.path.exists(inp.path2qm+inp.coefdir):
+        os.mkdir(inp.path2qm+inp.coefdir)
+    if not os.path.exists(inp.path2qm+inp.projdir):
+        os.mkdir(inp.path2qm+inp.projdir)
+
+xyzfile = read(inp.filename,":")
+ndata = len(xyzfile)
+
+# Distribute structures to tasks
+if inp.parallel:
+    if rank == 0:
+        conf_range = [[] for _ in range(size)]
+        blocksize = int(round(ndata/np.float(size)))
+        for i in range(size):
+            if i == (size-1):
+                conf_range[i] = list(range(ndata))[i*blocksize:ndata]
+            else:
+                conf_range[i] = list(range(ndata))[i*blocksize:(i+1)*blocksize]
+    else:
+        conf_range = None
+    conf_range = comm.scatter(conf_range,root=0)
+else:
+    conf_range = list(range(ndata))
+
+for i in conf_range:
+    dirpath = inp.path2qm+'data/'+str(i+1)+'/'
+    idx = np.loadtxt(dirpath+'idx_prodbas.out').astype(int)
+    cs_list = np.loadtxt(dirpath+'prodbas_condon_shotley_list.out').astype(int)
+    idx -= 1
+    cs_list -= 1
+    idx = list(idx)
+    cs_list = list(cs_list)
+    o = np.loadtxt(dirpath+'ri_projections.out').reshape(-1)
+    t = np.loadtxt(dirpath+'ri_restart_coeffs_df.out').reshape(-1)
+    ovlp = np.loadtxt(dirpath+'ri_ovlp.out').reshape(-1)
+    
+    n = len(o)
+    ovlp = ovlp.reshape(n,n)
+    
+    for j in cs_list:
+        ovlp[j,:] *= -1
+        ovlp[:,j] *= -1
+        o[j] *= -1
+        t[j] *= -1
+    
+    o = o[idx]
+    t = t[idx]
+    ovlp = ovlp[idx,:]
+    ovlp = ovlp[:,idx]
+    np.save(inp.path2qm+inp.ovlpdir+'overlap_conf'+str(i)+'.npy',ovlp)
+    np.save(inp.path2qm+inp.projdir+'projections_conf'+str(i)+'.npy',o)
+    np.save(inp.path2qm+inp.coefdir+'coefficients_conf'+str(i)+'.npy',t)
+    os.remove(dirpath+'ri_ovlp.out')
+    os.remove(dirpath+'ri_projections.out')
