9ff31da204e6eacdef26d1ab673f01250fdaa234
Zekun Lou <zekun.lou@mpsd.mpg.de>
2024-01-22 22:11:58 +0100

    fix path: equirepr.py and wigner.py

--- salted/wigner.py
+++ salted/wigner.py
@@ -1,7 +1,9 @@
 import os
 import sys
-import ase
 import time
+import os.path as osp
+
+import ase
 import numpy as np
 from sympy.physics.wigner import wigner_3j
 
@@ -12,21 +14,21 @@ from salted import basis
 def build(field):
     sys.path.insert(0, './')
     import inp
-    
+
     from salted.sys_utils import read_system, get_atom_idx
     species, lmax, nmax, lmax_max, nnmax, ndata, atomic_symbols, natoms, natmax = read_system()
     atom_idx, natom_dict = get_atom_idx(ndata,natoms,species,atomic_symbols)
-    
-    # Generate directories for saving descriptors 
+
+    # Generate directories for saving descriptors
     dirpath = os.path.join(inp.saltedpath, "wigners")
     if not os.path.exists(dirpath):
         os.mkdir(dirpath)
-    
+
     # Compute equivariant descriptors for each lambda value entering the SPH expansion of the electron density
     for lam in range(lmax_max+1):
-    
-        print("lambda =", lam)
-    
+
+        print(f"lambda = {lam}")
+
         # External field?
         if field:
             # Select relevant angular components for equivariant descriptor calculation
@@ -49,16 +51,18 @@ def build(field):
                         if abs(l2-lam) <= l1 and l1 <= (l2+lam) :
                             lvalues[llmax] = [l1,l2]
                             llmax+=1
-    
+
         # Fill dense array from dictionary
         llvec = np.zeros((llmax,2),int)
-        for il in range(llmax): 
+        for il in range(llmax):
             llvec[il,0] = lvalues[il][0]
             llvec[il,1] = lvalues[il][1]
-        
-        # Precompute Wigner-3J symbols and save to file as dense arrays 
+
+        # Precompute Wigner-3J symbols and save to file as dense arrays
         if field:
-            wig = open(inp.saltedpath+"wigners/wigner_lam-"+str(lam)+"_lmax1-"+str(inp.nang1)+"_field.dat","a")
+            wig = open(osp.join(
+                inp.saltedpath, "wigners", f"wigner_lam-{lam}_lmax1-{inp.nang1}_field.dat"
+            ), "a")
             iwig = 0
             for il in range(llmax):
                 l1 = lvalues[il][0]
@@ -74,7 +78,9 @@ def build(field):
                             print(float(w3j),file=wig)
             wig.close()
         else:
-            wig = open(inp.saltedpath+"wigners/wigner_lam-"+str(lam)+"_lmax1-"+str(inp.nang1)+"_lmax2-"+str(inp.nang2)+".dat","a")
+            wig = open(osp.join(
+                inp.saltedpath, "wigners", f"wigner_lam-{lam}_lmax1-{inp.nang1}_lmax2-{inp.nang2}.dat"
+            ), "a")
             iwig = 0
             for il in range(llmax):
                 l1 = lvalues[il][0]
