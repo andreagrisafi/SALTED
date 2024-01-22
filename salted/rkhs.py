e002b6568ea8a8bbc22d6fb27ca52d506856aa03
Zekun Lou <zekun.lou@mpsd.mpg.de>
2024-01-22 22:34:08 +0100

    fix rkhs.py: add size=0 for non-parallel case

--- salted/rkhs.py
+++ salted/rkhs.py
@@ -9,7 +9,7 @@ def build():
 
     sys.path.insert(0, './')
     import inp
-    
+
     if inp.parallel:
         from mpi4py import MPI
         # MPI information
@@ -19,19 +19,20 @@ def build():
     #    print('This is task',rank+1,'of',size)
     else:
         rank = 0
-    
+        size = 0
+
     saltedname = inp.saltedname
-    
+
     if inp.field==True:
         kdir = inp.saltedpath+"kernels_"+saltedname+"_field"
     else:
         kdir = inp.saltedpath+"kernels_"+saltedname
     kdir += '/'
-    
+
     species, lmax, nmax, llmax, nnmax, ndata, atomic_symbols, natoms, natmax = read_system()
-    
+
     atom_idx, natom_dict = get_atom_idx(ndata,natoms,species,atomic_symbols)
-    
+
     # number of sparse environments
     M = inp.Menv
     zeta = inp.z
@@ -41,7 +42,7 @@ def build():
         print("zeta =", zeta)
         print("Computing RKHS of symmetry-adapted sparse kernel approximations...")
     sdir = inp.saltedpath+'equirepr_'+inp.saltedname+'/'
-    
+
     # Distribute structures to tasks
     if inp.parallel:
         conf_range = get_conf_range(rank,size,ndata,list(range(ndata)))
@@ -49,29 +50,29 @@ def build():
         print('Task',rank+1,'handles the following structures:',conf_range,flush=True)
     else:
         conf_range = list(range(ndata))
-    
+
     power_env_sparse = {}
     if inp.field: power_env_sparse2 = {}
     kernel0_nm = {}
-    
+
     power = h5py.File(sdir+"FEAT-0.h5",'r')["descriptor"][conf_range,:]
     nfeat = power.shape[-1]
     if inp.field:
         power2 = h5py.File(sdir+"FEAT-0_field.h5",'r')["descriptor"][conf_range,:]
         nfeat2 = power2.shape[-1]
     Mspe = {}
-    
+
     for spe in species:
         if rank == 0: print("lambda = 0", "species:", spe)
         start = time.time()
-    
-        # compute sparse kernel K_MM for each atomic species 
+
+        # compute sparse kernel K_MM for each atomic species
         power_env_sparse[spe] = h5py.File(sdir+"FEAT-0-M-"+str(M)+".h5",'r')[spe][:]
         if inp.field: power_env_sparse2[spe] = h5py.File(sdir+"FEAT-0-M-"+str(M)+"_field.h5",'r')[spe][:]
         Mspe[spe] = power_env_sparse[spe].shape[0]
-    
+
         V = np.load(kdir+"spe"+str(spe)+"_l"+str(0)+"/M"+str(M)+"_zeta"+str(zeta)+"/projector.npy")
-        
+
         # compute feature vector Phi associated with the RKHS of K_NM * K_MM^-1 * K_NM^T
         for i,iconf in enumerate(conf_range):
             kernel0_nm[(iconf,spe)] = np.dot(power[i,atom_idx[(iconf,spe)]],power_env_sparse[spe].T)
@@ -84,10 +85,10 @@ def build():
             psi_nm = np.real(np.dot(kernel_nm,V))
             np.save(kdir+"spe"+str(spe)+"_l"+str(0)+"/M"+str(M)+"_zeta"+str(zeta)+"/psi-nm_conf"+str(iconf)+".npy",psi_nm)
         if rank == 0: print((time.time()-start)/60.0)
-    
+
     # lambda>0
     for l in range(1,llmax+1):
-    
+
         # load power spectrum
         if rank == 0: print("loading lambda =", l)
         power = h5py.File(sdir+"FEAT-"+str(l)+".h5",'r')["descriptor"][conf_range,:]
@@ -95,32 +96,32 @@ def build():
         if inp.field:
             power2 = h5py.File(sdir+"FEAT-"+str(l)+"_field.h5",'r')["descriptor"][conf_range,:]
             nfeat2 = power2.shape[-1]
-    
+
         for spe in species:
             if rank == 0: print("lambda = ", l, "species:", spe)
             start = time.time()
-    
+
             # get sparse feature vector for each atomic species
             power_env_sparse[spe] = h5py.File(sdir+"FEAT-"+str(l)+"-M-"+str(M)+".h5",'r')[spe][:]
             if inp.field: power_env_sparse2[spe] = h5py.File(sdir+"FEAT-"+str(l)+"-M-"+str(M)+"_field.h5",'r')[spe][:]
-            V = np.load(kdir+"spe"+str(spe)+"_l"+str(l)+"/M"+str(M)+"_zeta"+str(zeta)+"/projector.npy") 
-    
+            V = np.load(kdir+"spe"+str(spe)+"_l"+str(l)+"/M"+str(M)+"_zeta"+str(zeta)+"/projector.npy")
+
             # compute feature vector Phi associated with the RKHS of K_NM * K_MM^-1 * K_NM^T
             if zeta==1:
                 for i,iconf in enumerate(conf_range):
-                    if inp.field: 
-                        kernel_nm = np.dot(power2[i,atom_idx[(iconf,spe)]].reshape(natom_dict[(iconf,spe)]*(2*l+1),nfeat2),power_env_sparse2[spe].T) 
+                    if inp.field:
+                        kernel_nm = np.dot(power2[i,atom_idx[(iconf,spe)]].reshape(natom_dict[(iconf,spe)]*(2*l+1),nfeat2),power_env_sparse2[spe].T)
                     else:
-                        kernel_nm = np.dot(power[i,atom_idx[(iconf,spe)]].reshape(natom_dict[(iconf,spe)]*(2*l+1),nfeat),power_env_sparse[spe].T) 
+                        kernel_nm = np.dot(power[i,atom_idx[(iconf,spe)]].reshape(natom_dict[(iconf,spe)]*(2*l+1),nfeat),power_env_sparse[spe].T)
                     psi_nm = np.real(np.dot(kernel_nm,V))
                     np.save(kdir+"spe"+str(spe)+"_l"+str(l)+"/M"+str(M)+"_zeta"+str(zeta)+"/psi-nm_conf"+str(iconf)+".npy",psi_nm)
             else:
                 for i,iconf in enumerate(conf_range):
-                    if inp.field: 
+                    if inp.field:
                         kernel_nm = np.dot(power2[i,atom_idx[(iconf,spe)]].reshape(natom_dict[(iconf,spe)]*(2*l+1),nfeat2),power_env_sparse2[spe].T)
-                    else: 
+                    else:
                         kernel_nm = np.dot(power[i,atom_idx[(iconf,spe)]].reshape(natom_dict[(iconf,spe)]*(2*l+1),nfeat),power_env_sparse[spe].T)
-                    
+
                     for i1 in range(natom_dict[(iconf,spe)]):
                         for i2 in range(Mspe[spe]):
                             kernel_nm[i1*(2*l+1):i1*(2*l+1)+2*l+1][:,i2*(2*l+1):i2*(2*l+1)+2*l+1] *= kernel0_nm[(iconf,spe)][i1,i2]**(zeta-1)
