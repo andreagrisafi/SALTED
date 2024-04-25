785351e215be61ca18bfb48a0633f66cf8215800
andrea <andrea.grisafi91@gmail.com>
2024-04-25 13:47:12 +0200

    cp2k workflow and examples cleaned up

--- example/Au100-Na-CP2K/run.py
+++
@@ -1,9 +0,0 @@
-from salted import equirepr, sparsify, rkhs, feature_vector, matrices, regression, validation
-
-equirepr.build()
-sparsify.build()
-rkhs.build()
-feature_vector.build()
-matrices.build()
-regression.build()
-validation.build()
