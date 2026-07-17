221ac90c6e9eefd8850c1e2dfd3cb51b281ea5b5
Theophane Bernhard <theophane.bernhard@sorbonne-universite.fr>
2026-07-17 16:37:01 +0200

    Fixes

--- salted/prediction.py
+++ salted/prediction.py
@@ -278,7 +278,7 @@ def build():
 
             if qmcode=="cp2k":
                 # Compute charges and dipole moments
-                charge, dipole = compute_charge_and_dipole(inp.qm.pseudocharge,natoms[iconf],np.arange(natoms),atomic_symbols[iconf],atomic_coords[iconf],lmax,nmax,species,charge_integrals,dipole_integrals,pred_coefs,average,parallel,comm)
+                charge, dipole = compute_charge_and_dipole(inp.qm.pseudocharge,natoms[iconf],np.arange(natoms[iconf]),atomic_symbols[iconf],atomic_coords[iconf],lmax,nmax,species,charge_integrals,dipole_integrals,pred_coefs,average,parallel,comm)
                 print(iconf+1,charge,file=qfile)
                 print(iconf+1,dipole["x"],dipole["y"],dipole["z"],file=dfile)
             
