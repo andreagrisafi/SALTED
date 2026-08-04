95b0a59af1140497bc19a105d4a0058264fb871b
Alan M. Lewis <alanlewis372@gmail.com>
2026-08-04 12:12:10 +0100

    Add parallel test script to the response example

---
+++ example/water_monomer_AIMS_response/run-SALTED-parallel.sh
@@ -0,0 +1,19 @@
+# Setup SALTED calculation using AIMS data
+python3 -m salted.get_basis_info
+python3 -m salted.aims.move_data
+
+# Run full SALTED workflow
+python3 -m salted.initialize
+python3 -m salted.sparse_selection
+mpirun -np 4 python3 -m salted.sparse_descriptor
+python3 -m salted.rkhs_projector
+mpirun -np 4 python3 -m salted.rkhs_vector
+
+# For small dimensionality
+mpirun -np 4 python3 -m salted.hessian_matrix
+python3 -m salted.solve_regression
+
+# For high dimensionality
+#mpirun -np 4 python3 -m salted.minimize_loss
+
+mpirun -np 4 python3 -m salted.validation
