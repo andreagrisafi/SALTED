0615b679bb41b4f4f865bb4d6fb75beb2b3cd054
andreagrisafi <andrea.grisafi91@gmail.com>
2026-08-19 11:42:38 +0200

    constants expanded

--- salted/constants.py
+++ salted/constants.py
@@ -1,2 +1,3 @@
 bohr2angs = 0.529177210903 # From Bohr to Angstrom 
-hart2kcal = 627.5096080306 # From Hartree to kcal
+hart2kcal = 627.5096080306 # From Hartree to kcal/mol
+hart2ev   = 27.211399      # From Hartree to eV
