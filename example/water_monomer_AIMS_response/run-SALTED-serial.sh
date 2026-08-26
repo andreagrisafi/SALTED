106f0a6b488fa1d7080c4db78eb93853d399f5f0
Alan M Lewis <36488126+alanmlewis@users.noreply.github.com>
2026-08-26 15:07:58 +0100

    DFPT centralization (#108)
    * Update example to be consistent with changes to AIMS DFPT routines
    
    * Add parallel test script to the response example
    
    * Replace 1k dataset with 100 molecule dataset
    
    * Add support for loading SALTED density-response predictions
    
    * Density fitting method in reciprocal space
    
    * rebase + test_cube
    
    * Default grid cube_reconstruction
    
    * Minor fixes
    
    * Polishing
    
    * polishing
    
    * Sort G vector
    
    * Calculation of overlap matrices from primitives, followed by contraction
    
    * Truncation of G vector
    
    * Vectorization of cp2k2salted.py
    
    * Minor fixes to remove ParseConfig().get_all_params()
    
    * Minor optimization of gto_rec
    
    * Optimization and split of S and w calculating functions
    
    * Identity-metric overlap matrix in real-space with pyscf
    
    * Minor fixes on cube_reconstruction.py
    
    * Reciprocal-space part of the Coulomb-metric overlap matrix (overlap_coulomb_rec)
    
    * elec_energy_forces routine called from validation.py
    
    * Save pseudopotential in basis directory
    
    * Minor fixes
    
    * Revised water_monomer_CP2K example
    
    * Revised water_monomer_CP2K example: include local_pseudo
    
    * Real-space part of the Coulomb-metric overlap matrix (overlap_coulomb_real)
    
    * Reordering of scaling factors of S and w
    
    * Updated documentation
    
    * polish density_fitting function and add test_hartree.py in CP2K example
    
    * df time of linear solution added
    
    * Add 4pi factor of the Coulomb metric in get_w_prim
    
    * gto_rec: G-vector truncation based on omega
    
    * Revised variable names
    
    * gto_rec_prim: G-vector truncation per primitive
    
    * Minor fix
    
    * Hartree energy in density_fitting
    
    * get_w_prim optimization
    
    * Core charge density in real space
    
    * overlap_coulomb_rec: minor fixes
    
    * Electrostatic energy: Ewald implementation for the electron-nucleus term
    
    * Revised omega, gmax_omega, rcut_pairs
    
    * Minor fixes
    
    * Consistent naming
    
    * Revised Ewald screening approach
    
    * Ewald for e_en and e_ee correction
    
    * New function: compute_hartree_energy
    
    * Optimized cube file reading
    
    * updated cp2k validation with Hartree new calculator
    
    * Linear solve optimization
    
    * Numba implementation for get_w_prim
    
    * dfmetric input keyword polished
    
    * Minor optimizations
    
    * Code clean-up
    
    * Update documentation
    
    * CP2K README update plus minor polishing
    
    * minor README fix
    
    * improve CP2K docs
    
    * docs expanded and salted constants added
    
    * small fix
    
    * docs polishing
    
    * constants expanded
    
    * better total charge constraint for Hartree energy
    
    * Update AIMS example to make it run more smoothly. (#112)
    
    * Update AIMS example to make it run more smoothly.
    This includes only loading cp2k helper functions when qmcode==cp2k
    
    * Remove out-of-date submission script
    
    * Update rho1 outputs to have consistent naming conventions with rho
    
    * Add Note in README to specify AIMS version
    
    * Update README
    
    * Update run-SALTED-parallel.sh
    
    Remove repeated move_data
    
    * Fix bug, add reorder capability for density-response
    
    ---------
    
    Co-authored-by: Theophane Bernhard <theophane.bernhard@sorbonne-universite.fr>
    Co-authored-by: Agustin Salcedo <agustin.salcedo@phys.ens.fr>
    Co-authored-by: agrisafi <agrisafi@c1383.hostmgmt.cm.adastra.cines.fr>
    Co-authored-by: andreagrisafi <agrisafi@login5.head.cm.adastra.cines.fr>
    Co-authored-by: andreagrisafi <andrea.grisafi91@gmail.com>
    Co-authored-by: Andrea Grisafi <andreagrisafi@Mac.homenet.telecomitalia.it>

--- example/water_monomer_AIMS_response/run-SALTED-serial.sh
+++ example/water_monomer_AIMS_response/run-SALTED-serial.sh
@@ -1,8 +1,20 @@
+# Setup SALTED calculation using AIMS data
+python3 -m salted.get_basis_info
+
+# Run full SALTED workflow
 python3 -m salted.initialize
 python3 -m salted.sparse_selection
 python3 -m salted.sparse_descriptor
 python3 -m salted.rkhs_projector
 python3 -m salted.rkhs_vector
+
+# For small dimensionality
 python3 -m salted.hessian_matrix
 python3 -m salted.solve_regression
+
+# For high dimensionality
+#python3 -m salted.minimize_loss
+
 python3 -m salted.validation
+
+python3 -m salted.prediction
